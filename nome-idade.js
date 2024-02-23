c4e4bc0821b988c29f4ae6893152fb6135420586
julinhafmv <128698307+julinhafmv@users.noreply.github.com>
2024-02-23 12:01:23 -0300

    Add files via upload

---
+++ nome-idade.js
@@ -0,0 +1,10 @@
+let nome = "julia";
+let inteiro = 18;
+let inteiro2 = 7;
+let peso = 43.2;
+let altura = 1.60;
+var n1 = 
+
+console.log(`a idade de ${nome} é ${inteiro + inteiro2}`);
+console.log(` a altura de ${nome} é ${altura} e pesa ${peso}`);
+
