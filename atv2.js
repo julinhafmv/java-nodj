7c53034c7b93cfb4e777fe1f27867bfb95299602
julinhafmv <128698307+julinhafmv@users.noreply.github.com>
2024-03-04 11:35:28 -0300

    Add files via upload

---
+++ atv2.js
@@ -0,0 +1,39 @@
+let pessoa1 = {
+    nome: `Julinha`,
+    idade: 18,
+    habilitacao: `Habilitado`,
+    autorizacao: `Autorizado`,
+
+}; if (pessoa1.idade >= 18 && pessoa1.habilitacao == true  && pessoa1.autorizacao == true ) {
+    console.log(`Habilitada e autorizada`)
+}else if (pessoa1.idade >= 18 && pessoa1.autorizacao == true);
+
+let pessoa2 = {
+    nome: `Hilaine Mensh`,
+    idade: 97,
+    habilitacao: `Habilitado`,
+    autorizacao: `Não-Autorizado`,
+
+}; if (pessoa2.idade >= 18 && pessoa2.habilitacao == true  && pessoa2.autorizacao == true ) {
+    console.log(`Habilitada e autorizada`)
+}else if (pessoa2.idade >= 18 && pessoa2.autorizacao == false );
+
+
+
+
+let pessoa3 = {
+    nome: `Tevegosh`,
+    idade: 17,
+    habilitacao: `Nao-Habilitado`,
+    autorizacao: `Nao-Autorizado`,
+
+}; if (pessoa3.idade >= 18 && pessoa3.habilitacao == true && pessoa3.autorizacao == true ) {
+    console.log(`Habilitada e autorizada`)
+}else if (pessoa3.idade >= 18 && pessoa3.autorizacao == false ) 
+
+
+
+
+
+console.log(` O(A) ${pessoa1.nome}, tem ${pessoa1.idade} anos, atualmente ${pessoa1.habilitacao} para dirigir, e ${pessoa1.autorizacao} para entrar no evento`)
+
---
+++ resoluções.js
@@ -0,0 +1,59 @@
+/*Questão 1:
+
+let numero = 15.350;
+
+let numero1 = 2.000 + 0.350 + 13.000
+
+let numero2 = 0 + 2.000
+
+let numero3 = numero2 + 0.350
+
+let numero4 = numero3 + 15.000
+console.log(numero2.toFixed(3)
+*/
+
+/*Questão 2:
+
+let num1 = -50 - 50;
+
+let num2 = num1 - 100;
+
+let num3 = num2 -200;
+
+let num4 = num3 - 100;
+
+let numero5 = num4 - 67;
+console.log(num5)
+*/
+
+/*Questão 3:
+
+let num1 = 490 * 2  ;
+
+let num2 = num1 * 1; 
+console.log(num2)
+*/
+
+/*Questão 4:
+
+let num1 = 567 / 3;
+
+let num2 = num1 / 3;
+
+let num3 = num2 / 3;
+console.log(num3)
+*/
+
+/*Questão 5:
+
+let num1 = 3000 % 90;
+
+let num2 = 90 % 40;
+
+let num3 = 40 % 30; 
+
+let num4 = 30 % 8;
+
+console.log(num4)
+console.log(num1 + num2 + num3 +num4 )
+*/
